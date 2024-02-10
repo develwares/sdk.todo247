0c4b30072802853f8241d46c72490a3169c949fc
cvisbal0724 <cvisbal0724@gmail.com>
2024-02-09 20:26:31 -0500

    config

--- repositories/carrier/transport.repository.js
+++ repositories/carrier/transport.repository.js
@@ -4,10 +4,10 @@ exports.TransportRepository = void 0;
 const base_repository_1 = require("../base.repository");
 class TransportRepository extends base_repository_1.BaseRepository {
     async currentByUser(userId, userToken) {
-        return this.http.get(this.apiUrl, { userId }, { 'Authorization': `Bearer ${userToken}` });
+        return this.http.get(`${this.apiUrl}/by-user/${userId}`, {}, { 'Authorization': `Bearer ${userToken}` });
     }
     async currentByCarrier(carrierId, userToken) {
-        return this.http.get(this.apiUrl, { carrierId }, { 'Authorization': `Bearer ${userToken}` });
+        return this.http.get(`${this.apiUrl}/by-carrier/${carrierId}`, {}, { 'Authorization': `Bearer ${userToken}` });
     }
 }
 exports.TransportRepository = TransportRepository;
--- repositories/security/user.repository.js
+++ repositories/security/user.repository.js
@@ -4,7 +4,7 @@ exports.UserRepository = void 0;
 const base_repository_1 = require("../base.repository");
 class UserRepository extends base_repository_1.BaseRepository {
     async getProfile(userToken) {
-        return this.http.get(this.apiUrl, {}, { 'Authorization': `Bearer ${userToken}` });
+        return this.http.get(`${this.apiUrl}/get/profile`, {}, { 'Authorization': `Bearer ${userToken}` });
     }
 }
 exports.UserRepository = UserRepository;
